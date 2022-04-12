38bd4e9d40051445bb8f65881f74db4ee2dfb695
Hector <hector.keith91@gmail.com>
2022-04-12 11:53:10 -0400

    started basic styling and html structure.  defined values through javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+let startingCount = 0;
+let countBy = 0;
+
+let startingCountUI = document.querySelector("#current-count");
+let countByUI = document.querySelector("#count-by");
+startingCountUI.textContent = startingCount;
+countByUI.textContent = countBy;
