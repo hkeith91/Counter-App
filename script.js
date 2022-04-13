6ccbdc0d48a9bd82361e2134eebc7a56e9c8108c
Hector <hector.keith91@gmail.com>
2022-04-13 10:03:49 -0400

    added single unit increment and decrement

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let startingCount = 0;
 let countBy = 1;
 
-let startingCountUI = document.querySelector("#current-count");
-let countByUI = document.querySelector("#count-by");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const startingCountUI = document.querySelector("#current-count");
+const countByUI = document.querySelector("#count-by");
 startingCountUI.textContent = startingCount;
 countByUI.textContent = countBy;
+
+plus.addEventListener("click", e =>{
+    startingCount += countBy;
+    startingCountUI.textContent = startingCount;
+});
+
+minus.addEventListener("click", e =>{
+    if(startingCountUI === 0) return;
+    startingCount -= countBy;
+    startingCountUI.textContent = startingCount;
+})
+
